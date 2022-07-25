905c6036fc0596affd62842500a347e542dfb75f
hapy <251503335@qq.com>
2022-07-25 15:42:27 +0800

    svm

--- algorithm/svm/main.py
+++ algorithm/svm/main.py
@@ -63,6 +63,10 @@ class SVMSample:
     def lookSVCs(self):
         x_min, x_max = self.xserials.min() - 1, self.xserials.max() + 1
         y_min, y_max = self.yserials.min() - 1, self.yserials.max() + 1
+        
+        '''
+        meshgrid 从坐标向量中返回坐标矩阵 [https://blog.csdn.net/littlehaes/article/details/83543459]
+        '''
         xx, yy = np.meshgrid(np.arange(x_min, x_max, 0.02), np.arange(y_min, y_max, 0.02))
         
         for i, C in enumerate((0.1, 1, 10)):
@@ -70,6 +74,12 @@ class SVMSample:
                 plt.subplot()
                 clf = SVC(C=C, gamma=gamma)
                 clf.fit(self.X, self.y)
+                '''
+                ravel 降为一维 [https://blog.csdn.net/hanshuobest/article/details/78882425]
+                np.c_ 坐标点的X值序列、Y值序列 转换成 坐标点二维矩阵
+                
+                关于predict, predict_proba [https://www.pythonheidong.com/blog/article/878018/cb35f513c9158417fa57/]
+                '''
                 Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
 
                 # Put the result into a color plot
