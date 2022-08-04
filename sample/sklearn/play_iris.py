7d606bce75815537196701f49baed3cf88445838
hapy <251503335@qq.com>
2022-08-04 18:03:54 +0800

    play...

--- sample/sklearn/play_iris.py
+++ sample/sklearn/play_iris.py
@@ -3,6 +3,7 @@ import logging
 import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
+import sklearn.decomposition as decomposition
 from sklearn import datasets, svm
 from sklearn.svm import SVC
 from sklearn.datasets import make_moons, make_circles, make_classification
@@ -12,6 +13,12 @@ from matplotlib.colors import ListedColormap
 
 '''
 https://zhuanlan.zhihu.com/p/31785188
+
+sklearn
+https://scikit-learn.org/stable/auto_examples/datasets/plot_iris_dataset.html#sphx-glr-auto-examples-datasets-plot-iris-dataset-py
+
+matplotlib API
+https://matplotlib.org/stable/api/_as_gen/matplotlib.axes.Axes.scatter.html
 '''
 
 __all__ = ['Sample']
@@ -33,7 +40,12 @@ class Sample:
         logging.info('setup ...')
         
         # matplotlib.use('TkAgg')
-        
+
+    def teardown(self):
+        logging.info('teardown ...')
+        pass
+    
+    def show2D_Sepal(self):
         iris = datasets.load_iris()
         features = iris.data
         target = iris.target
@@ -47,92 +59,100 @@ class Sample:
         self.cm_bright = ListedColormap(['#FF0000', '#0000FF', '#00FF00'])
         ax = plt.subplot()
 
-        ax.set_title("Input data")
+        ax.set_title("")
+        # Plot the training points
+        self.xserials = self.X[:, 0]
+        self.yserials = self.X[:, 1]
+        ax.scatter(self.xserials, self.yserials, c=self.y, cmap=self.cm_bright)
+        ax.set_xticks(range(int(self.xserials.min() - 1), int(self.xserials.max() + 1)))
+        ax.set_yticks(range(int(self.yserials.min() - 1), int(self.yserials.max() + 1)))
+        plt.xlabel("Sepal length")
+        plt.ylabel("Sepal width")
+        plt.tight_layout()
+        plt.show()
+    
+    def show2D_Petal(self):
+        iris = datasets.load_iris()
+        features = iris.data
+        target = iris.target
+        logging.info('{} {}'.format(features.shape,target.shape))
+        logging.info('{}'.format(iris.feature_names))
+        
+        self.X, self.y = features, target
+        # self.X = StandardScaler().fit_transform(self.X)
+        
+        cm = plt.cm.RdBu
+        self.cm_bright = ListedColormap(['#FF0000', '#0000FF', '#00FF00'])
+        ax = plt.subplot()
+
+        ax.set_title("")
         # Plot the training points
         self.xserials = self.X[:, 2]
         self.yserials = self.X[:, 3]
         ax.scatter(self.xserials, self.yserials, c=self.y, cmap=self.cm_bright)
         ax.set_xticks(range(int(self.xserials.min() - 1), int(self.xserials.max() + 1)))
         ax.set_yticks(range(int(self.yserials.min() - 1), int(self.yserials.max() + 1)))
+        plt.xlabel("Petal length")
+        plt.ylabel("Petal width")
         plt.tight_layout()
         plt.show()
-
-    def teardown(self):
-        logging.info('teardown ...')
-        pass
     
-    def lookSVCBest(self):
-        grid = GridSearchCV(SVC(), param_grid={"C":[0.1, 1, 10], "gamma": [1, 0.1, 0.01]}, cv=4)
-        grid.fit(self.X, self.y)
-        logging.info("The best parameters are {} with a score of {:0.2f}".format(grid.best_params_, grid.best_score_))
-    
-    def lookSVCs(self):
-        x_min, x_max = self.xserials.min() - 1, self.xserials.max() + 1
-        y_min, y_max = self.yserials.min() - 1, self.yserials.max() + 1
+    def show3D_(self):
+        # import some data to play with
+        iris = datasets.load_iris()
+        X = iris.data[:, :2]  # we only take the first two features.
+        y = iris.target
+
+        x_min, x_max = X[:, 0].min() - 0.5, X[:, 0].max() + 0.5
+        y_min, y_max = X[:, 1].min() - 0.5, X[:, 1].max() + 0.5
+
+        fig = plt.figure(figsize=(8, 16))
+        ax1 = fig.add_subplot(211, xlim=(x_min, x_max), ylim=(y_min, y_max))
+
+        # Plot the training points
+        ax1.scatter(X[:, 0], X[:, 1], c=y, cmap=plt.cm.Set1, edgecolor="k")
+        ax1.set_xlabel("Sepal length")
+        ax1.set_ylabel("Sepal width")
+
+        # ax1.xlim(x_min, x_max)
+        # ax1.ylim(y_min, y_max)
+        # ax1.xticks(())
+        # ax1.yticks(())
+
+        # To getter a better understanding of interaction of the dimensions
+        # plot the first three PCA dimensions
+        # fig = plt.figure(1, figsize=(8, 6))
+        ax2 = fig.add_subplot(212, projection="3d")
+
+        X_reduced = decomposition.PCA(n_components=3).fit_transform(iris.data)
+        ax2.scatter(
+            X_reduced[:, 0],
+            X_reduced[:, 1],
+            X_reduced[:, 2],
+            c=y,
+            cmap=plt.cm.Set1,
+            edgecolor="k",
+            s=40,
+        )
+        ax2.set_title("First three PCA directions")
+        ax2.set_xlabel("1st eigenvector")
+        ax2.w_xaxis.set_ticklabels([])
+        ax2.set_ylabel("2nd eigenvector")
+        ax2.w_yaxis.set_ticklabels([])
+        ax2.set_zlabel("3rd eigenvector")
+        ax2.w_zaxis.set_ticklabels([])
         
-        '''
-        meshgrid 从坐标向量中返回坐标矩阵 [https://blog.csdn.net/littlehaes/article/details/83543459]
-        '''
-        xx, yy = np.meshgrid(np.arange(x_min, x_max, 0.02), np.arange(y_min, y_max, 0.02))
-        
-        for i, C in enumerate((0.1, 1, 10)):
-            for j, gamma in enumerate((1, 0.1, 0.01)):
-                plt.subplot()
-                '''
-                kernel
-                常见的几中核函数 [https://blog.csdn.net/qq_37007384/article/details/88418256]
-                    - rbf [https://blog.csdn.net/qq_36264495/article/details/88316482]
-                SVC 参数
-                https://blog.csdn.net/qq_37007384/article/details/88410998
-                
-                高斯核
-                有人说，高斯核函数的本质是将每一个样本点映射到一个无穷维的特征空间
-                http://t.zoukankan.com/jokingremarks-p-14337248.html
-                https://zhuanlan.zhihu.com/p/258047472
-                '''
-                clf = SVC(C=C, gamma=gamma, kernel='rbf')
-                clf.fit(self.X, self.y)
-                '''
-                ravel 降为一维 [https://blog.csdn.net/hanshuobest/article/details/78882425]
-                np.c_ 坐标点的X值序列、Y值序列 转换成 坐标点二维矩阵
-                
-                关于predict, predict_proba [https://www.pythonheidong.com/blog/article/878018/cb35f513c9158417fa57/]
-                '''
-                Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
+        ax2.set_xlim(X_reduced[:, 0].min()-0.5, X_reduced[:, 0].max()+0.5)
+        ax2.set_ylim(X_reduced[:, 1].min()-0.5, X_reduced[:, 1].max()+0.5)
+        ax2.set_zlim(X_reduced[:, 2].min()-0.5, X_reduced[:, 2].max()+0.5)
 
-                '''
-                reshape 一维转多维 [https://numpy.org/doc/stable/reference/generated/numpy.reshape.html]
-                reshaper后的 Z 为xx.shape维的Z值
-                '''
-                # Put the result into a color plot
-                Z = Z.reshape(xx.shape)
-                
-                '''
-                (x, y) ->(z) , z 为预测值
-                类似于画出等高线 [https://blog.csdn.net/lens___/article/details/83960810]
-                '''
-                plt.contourf(xx, yy, Z, cmap=plt.cm.coolwarm, alpha=0.8)
-
-                '''
-                pyplot.scatter
-                https://blog.csdn.net/yuan_xiangjun/article/details/119514811
-                '''
-                # Plot also the training points
-                plt.scatter(self.xserials, self.yserials, c=self.y, cmap=self.cm_bright, marker='x')
-
-                plt.xlim(xx.min(), xx.max())
-                plt.ylim(yy.min(), yy.max())
-                plt.xticks(())
-                plt.yticks(())
-                plt.xlabel(" gamma=" + str(gamma) + " C=" + str(C))
-                plt.show()
-                
-                logging.info('')
+        plt.show()
     
 if __name__ == '__main__':
     sample = Sample()
     sample.setup(option={})
-    # sample.lookSVCBest()
-    sample.lookSVCs()
+    # sample.show2D_Sepal()
+    # sample.show2D_Petal()
+    sample.show3D_()
     
     logging.info('over')
